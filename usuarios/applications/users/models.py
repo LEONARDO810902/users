0c05858708713e7a76f9528d3b884b67a1305071
Leonardo Merchan <leonardo.merchan.pg@gmail.com>
2022-02-08 10:27:03 -0500

    Configuracion para la primera migrate

---
+++ usuarios/applications/users/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+
+# Create your models here.
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+
+    GENDER_CHOICES = (
+        ('M', 'Masculino'),
+        ('F', 'Femenino'),
+        ('O', 'Otros'),
+    )
+
+    username = models.CharField(max_length=15, unique=True)
+    email = models.EmailField()
+    nombres = models.CharField(max_length=30)
+    apellidos = models.CharField(max_length=30)
+    genero = models.CharField(max_length=1, choices=GENDER_CHOICES, blank=True)
+
+    USERNAME_FIELD = 'username'
+
+    def get_short_name(self):
+        return self.username
+
+    def get_short_name(self):
+        return self.nombres + ' ' + self.apellidos
--- usuarios/usuarios/settings/base.py
+++ usuarios/usuarios/settings/base.py
@@ -51,7 +51,9 @@ DJANGO_APPS = (
     'django.contrib.staticfiles',
 )
 
-LOCAL_APPS = ()
+LOCAL_APPS = (
+    'applications.users',
+)
 
 THIRD_PARTY_APPS = ()
 
@@ -106,6 +108,8 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+AUTH_USER_MODEL = 'users.User'
 
 
 # Internationalization
